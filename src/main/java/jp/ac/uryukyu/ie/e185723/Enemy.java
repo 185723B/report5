0a16bbf372b08c23cdceef20c4c16258bd3949d6
yusuke shimabukuro <e185723@ie.u-ryukyu.ac.jp>
2019-02-11 15:32:44 +0900

    first

---
+++ src/main/java/jp/ac/uryukyu/ie/e185723/Enemy.java
@@ -0,0 +1,19 @@
+package jp.ac.uryukyu.ie.e185723;
+
+
+public class Enemy extends LivingThing {
+
+    public Enemy() {
+        super("スライム",6,3);
+    }
+
+    @Override
+    public void wounded(int damage) {
+        setHitpoint(getHitpoint() - damage);
+        if( getHitpoint() < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた！\n",getName());
+        }
+    }
+}
+
---
+++ src/main/java/jp/ac/uryukyu/ie/e185723/Hero.java
@@ -0,0 +1,18 @@
+package jp.ac.uryukyu.ie.e185723;
+
+
+public class Hero extends LivingThing {
+
+    public Hero() {
+        super("勇者",10,5);
+    }
+
+    @Override
+    public void wounded(int damage) {
+        setHitpoint(getHitpoint() - damage);
+        if( getHitpoint() < 0 ) {
+            setDead(true);
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n",getName());
+        }
+    }
+}
